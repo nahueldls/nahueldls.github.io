54a85b3121c41f8afa168dee91cd46c9d4108432
Nahuel De Los Santos <nahueldelossantos646@gmail.com>
2022-08-27 18:29:38 -0300

    parte 1 de entrega individual.

--- js/init.js
+++ js/init.js
@@ -39,3 +39,6 @@ let getJSONData = function(url){
         return result;
     });
 }
+
+const nav = document.getElementById("local");
+nav.innerHTML = localStorage.getItem("email");
--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
-function myFunction() {
-    location.replace("https://nahueldls.github.io/portada.html")
+/* function myFunction() {
+    window.location.href = "https://nahueldls.github.io/portada.html"
 }
 const n = document.getElementById("name");
 const e = document.getElementById("email");
@@ -9,8 +9,21 @@ function redireccion(){
         myFunction()
     } else {
     }
-}
+} */
 
 function handleCredentialResponse() {
     location.replace("https://nahueldls.github.io/portada.html")
 }
+
+const e = document.getElementById("email");
+const p = document.getElementById("password");
+const button = document.getElementById("enviar");
+
+function validation() {
+    if ( e.value !== "" && p.value !== "") {
+        localStorage.setItem("email", e.value);
+        const mostra = localStorage.getItem("email")
+        location.href = "portada.html"
+    }
+}
+button.addEventListener("click", validation);
