e0e68b4c5487c9b68906f5559a7857bc957848bd
Nahuel De Los Santos <nahueldelossantos646@gmail.com>
2022-09-03 17:22:03 -0300

    add other filters

--- js/products.js
+++ js/products.js
@@ -2,36 +2,30 @@
 const id = localStorage.getItem("catID"); 
 const lista = await fetch(`https://japceibal.github.io/emercado-api/cats_products/${id}.json`)
                  .then(response => response.json());
-                 console.log(lista.products) //prueba de que funciona
+                //  console.log(productos[0]) //prueba de que funciona
                 
                  //  DOM,variables y funciones ------>
 
 
-let screen = undefined;
-const contenedor = document.getElementById("contenedor"); 
+let screen;
+const contenedor = document.getElementById("funciona"); 
 let productos = lista.products;
+console.log(productos[0])
+
+// BOTONES Y MÁS
 const buttonOrder = document.getElementById("sortAsc");
-
-/* function cambiar(){
-    screen = "costo";
-    mostrar()
-}
- buttonOrder.addEventListener("click", cambiar);
-
-function ordenar(b, a) {
-    return a.cost - b.cost;
-}
-
-function mostrar() {
-    if (screen == undefined) {crearHtml(productos)}
-    else {crearHtml(productos.sort(ordenar))}
-} */
+const buttonOrderBack = document.getElementById("sortDesc");
+const relevancia = document.getElementById("relevancia");
+const limpiar = document.getElementById("clearRangeFilter");
+// BOTONES Y MÁS
 
 function crearHtml(lista_de_productos) { 
 
+    contenedor.innerHTML = ""
+
     for( let i = 0; i < lista_de_productos.length; i++) {
         contenedor.innerHTML += `
-        <div onclick="setCatID(${lista_de_productos[i].id})" class="list-group-item list-group-item-action cursor-active">
+        <div class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
                         <img src="${lista_de_productos[i].image}" alt="autos" class="img-thumbnail">
@@ -50,33 +44,98 @@ function crearHtml(lista_de_productos) {
 
 }
 
+// crearHtml(lista.products)
+
+// window.onload = showHtml();
+
  function showHtml() {
-    if ( screen == undefined){
+    if( screen == undefined){
         crearHtml(productos)
-    }else if( screen == "costo"){
-        crearHtml(productos.sort(ordenar))
+    } else if( screen == "costo") {
+        crearHtml(productos.sort(function(a,b){
+            if( b.cost > a.cost){
+                return 1;
+            } else {
+                return -1;
+            }
+        }))
+    } else if ( screen == "costomenor") {
+        crearHtml(productos.sort(function(a,b){
+            if( b.cost > a.cost){
+                return -1;
+            } else {
+                return 1;
+            }
+        }))
+    } else if ( screen == "relevancia") {
+        crearHtml(productos.sort(function(a,b){
+            if( b.soldCount > a.soldCount){
+                return 1;
+            } else {
+                return -1;
+            }
+        }))
+    } else if ( inputMinimo != undefined && inputMinimo != "" && inputMaximo != undefined && inputMaximo != "") {
+        crearHtml(productos.sort(function(a, b) {
+
+        }))
     }
  }
  showHtml()
+  
 
-//  crear una funcion que cree el criterio de orden 
-
-function ordenar(a, b) {
-    return b.costo - a.costo;
-}
-
-//  crear una funcion que cambie el valor de la variable screen 
+//funciones de cambio de varible para el mostrado de pantalla
 
 function cambiar() {
     screen = "costo"
     showHtml();
 } //la funcion tiene que activarse cuando se toque el botón <Mayor precio>
-buttonOrder.addEventListener("click", cambiar)
+buttonOrder.addEventListener("click", cambiar) //botón
+
+function cambiarAlreves() {
+    screen = "costomenor"
+    showHtml();
+} //la funcion tiene que activarse cuando se toque el botón <Menor precio>
+
+buttonOrderBack.addEventListener("click", cambiarAlreves) //botón
+
+function relevanciaSc() {
+    screen = "relevancia"
+    showHtml()
+}
+ relevancia.addEventListener("click", relevanciaSc);
 
 
+     //FILTROS NÚMERICOS
 
-/* PROBELMA: LA FUNCIÓN CAMBIAR, LE DA UN VALOR NUEVO A LA VARIABLE SCREEN Y ORDENA EN FUNCIÓN DE COSTOS
-PERO NO REDIRECCIONA A LA FUNCIÓN showHtml PARA QUE EVALÚE EL VALOR DE SCREEN */
+     /* ideas generales --> - está relacionado con el <cost>.
+                            - tiene que guardar el valor recibido en su input.
+                            - convertir en un dato tipo number el costo.
+                            - analizar si está dentro del rango de precio(el costo)
+                              y a partir de eso mostrar los productos.
+                            algoritmo:
+qué es lo primero quq quiero hacer? -crear una function que reciba los input max y min,cambie el valor de la 
+variable screen y vaya hacia la function showHtml().
+
+*/
+
+
+                                 
+
+
+const filtrar = document.getElementById("rangeFilterCount"); //botón de filtrar
+const minimo = document.getElementById("rangeFilterCountMin"); //input de precio mínimo
+const maximo = document.getElementById("rangeFilterCountMax"); //input de precio máximo
+let inputMinimo = minimo.value; //valor de input (precio mínimo)
+let inputMaximo = maximo.value; //valor de input (precio máximo)
+
+
+function saveAndConvert() {
+    valorMinimo = parseInt(valorMinimo);
+    valorMaximo = parseInt(valorMaximo);
+    showHtml();
+}
+
 
 
 
@@ -87,24 +146,8 @@ PERO NO REDIRECCIONA A LA FUNCIÓN showHtml PARA QUE EVALÚE EL VALOR DE SCREEN 
  
  
  
- /* function ordenar(x, y) {
-    if (x.apellido < y.apellido) {return -1;}
-    if (x.apellido > y.apellido) {return 1;}
-    return 0;
-    la función observa lo que hace es comparar dos cosas(las que se toman por parametro) y ver cual es menor,
-    si la primera es menor que la segunda retorna un numero menor a uno y viceversa,al comparar todos va ubicando
-    de menor a mayor.
 
-   }
- */
 
-   /* function ordenar2(x, y) {
-    return x.apellido.localeCompare(y.apellido);
-   } 
-   let r = n.sort(ordenar2)
-   console.log(r) 
-   El método localeCompare () retorna un número indicando si una cadena de carateres de referencia va antes,
-   después o si es la misma que la cadena dada en orden alfabético.*/
 
 
 
